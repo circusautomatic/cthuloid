9f64bfb4774097a1ed6f82e691eb05979e50cc47
Francesco Capodieci <fgcapo@gmail.com>
2017-06-08 20:25:24 -0700

    update starting angles

--- Arduino/ServoShieldKeyboard/ServoShieldKeyboard.ino
+++ Arduino/ServoShieldKeyboard/ServoShieldKeyboard.ino
@@ -1,21 +1,3 @@
-/*************************************************** 
-  This is an example for our Adafruit 16-channel PWM & Servo driver
-  Servo test - this will drive 16 servos, one after the other
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/815
-
-  These displays use I2C to communicate, 2 pins are required to  
-  interface. For Arduino UNOs, thats SCL -> Analog 5, SDA -> Analog 4
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
@@ -33,9 +15,10 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 //angle for each servo in degrees
 volatile int16_t servoAngles[] =   {
-  90, 0, 180, 0,    // left shoulder, upperarm, forearm, hand
-  90, 180, 0, 0,    // right shoulder, upperarm, forearm, hand
-  0, 90, 0          // neck, nod, face
+  90, 90, 0,         // neck, nod, face
+  0, 0, 0, 0,      // left shoulder, upperarm, forearm, hand
+  180, 180, 180, 180    // right shoulder, upperarm, forearm, hand
+  
 };
 const int NUM_SERVOS = sizeof(servoAngles) / sizeof(servoAngles[0]);
 
