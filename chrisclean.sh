8a28d1371846cf1880f932ed44793f6d3caca1f0
Francesco Capodieci <fgcapo@gmail.com>
2017-08-09 18:28:37 -0700

    cleanup

---
+++ chrisclean.sh
@@ -0,0 +1,12 @@
+# For PCBs designed using KiCad: http://www.kicad-pcb.org/
+
+# Temporary files
+git rm -r *.000
+git rm -r *.bak
+git rm -r *.bck
+git rm -r *.kicad_pcb-bak
+git rm -r  *~ _autosave-*
+git rm -r  *.tmp __MACOSX*
+git rm -r  *.net
+git rm -r  *.dsn
+git rm -r  *.ses
