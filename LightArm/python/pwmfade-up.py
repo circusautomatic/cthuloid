bd577c6a7d919b793f84628bc3ea5342ccb8a27f
plankatron <fleeky@gmail.com>
2018-12-13 22:21:36 -0800

    adding a fade-up example

---
+++ LightArm/python/pwmfade-up.py
@@ -0,0 +1,37 @@
+# pwm fade over network socket
+
+import socket, math, time
+
+# Create a TCP/IP socket
+#server_addresses = [('10.0.2.3', 1337), ('10.0.2.4', 1337), ('10.0.2.6', 1337)]
+server_addresses = [('10.0.0.159', 1337)]
+sockets = []
+
+try:
+  for server_address in server_addresses:
+    print('connecting to', server_address)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(2.0)
+    sock.connect(server_address)
+    sockets.append(sock)
+
+  print('connected?')
+  interval = .01 #seconds
+  maxpwm = 65535
+  minpwm = 2000
+  inc = 200
+  i = minpwm
+  while i<=maxpwm:
+    for sock in sockets:
+      s = 'pwm ' + str(abs(i)) + '\n'
+#      print(s)
+      sock.sendall(bytes(s, 'UTF-8'))
+#      if i % 1000 == 0: print(i)
+
+      i += inc
+      if i >= maxpwm: sock.close()
+      time.sleep(interval)
+
+except socket.timeout:
+    print('error: connected timed out')
+
